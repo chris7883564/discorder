e363ae42c3dc5e7db6b84f7d8e1af4ef275efb42
chris7883564 <chris@musecopilot.com>
2024-04-18 12:51:18 +1000

    cleanup

--- src/commands/index.js
+++
@@ -1,5 +0,0 @@
-import eula from "./eula";
-import start from "./start";
-import stop from "./stop";
-export const commands = [start, stop, eula];
-//# sourceMappingURL=index.js.map
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import { bot } from "./bot";
-import { BOT_TOKEN } from "./config";
-bot.login(BOT_TOKEN);
-//# sourceMappingURL=index.js.map
