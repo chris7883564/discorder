7c594cbdd02c197f1d185f7f54e51d5b61ffe374
chris7883564 <chris@musecopilot.com>
2024-04-27 16:28:36 -0700

    working

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import { bot } from "./bot";
 import { BOT_TOKEN } from "./config";
 
+import { ConvexHttpClient } from "convex/browser";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
+const address = process.env.CONVEX_URL;
+
+const client: ConvexHttpClient = new ConvexHttpClient(String(address));
+// client.query(api.tasks.get).then((response: any) => console.log(response));
+
+// client.query(api.tasks.get).then((response) => console.log(response));
+
 bot.login(BOT_TOKEN);
