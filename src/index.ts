da758b6219da984a6a7e0cd440a4755e3e259b28
chris7883564 <chris@musecopilot.com>
2024-04-27 20:08:53 -0700

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { BOT_TOKEN } from "./config";
 
 bot.login(BOT_TOKEN);
 
-import { testSendAudio } from "./ctest";
+import { uploadFileToConvex } from "./recorder/convexuploader";
 // test1().then(console.log);
-testSendAudio().then(console.log);
+uploadFileToConvex("./data/recording/1714273057501/1007158516705927243/00000524.wav").then(
+	console.log,
+);
--- src/recorder/convexuploader.ts
+++ src/recorder/convexuploader.ts
@@ -8,7 +8,7 @@ log.enabled = true;
 import { ConvexHttpClient } from "convex/browser";
 import * as fs from "node:fs/promises";
 
-import { api } from "../convex/_generated/api";
+import { api } from "../../convex/_generated/api";
 
 const address = process.env.CONVEX_URL;
 const client = new ConvexHttpClient(String(address));
@@ -17,26 +17,19 @@ export function test1() {
 	return client.query(api.tasks.getTasks);
 }
 
-export async function testSendAudio() {
-	// e.g. https://happy-animal-123.convex.site/sendImage?author=User+123
+export async function uploadFileToConvex(filename: string) {
 	const uploadURL = await client.mutation(api.stems.generateUploadUrl);
 
-	log("**************");
-	log(uploadURL);
-	log("---------------------\n\n\n");
-
-	const filename = "./data/recording/1714273057501/1007158516705927243/00000524.wav";
+	// const filename = "./data/recording/1714273057501/1007158516705927243/00000524.wav";
 	const file = await fs.readFile(filename);
-	log("file", file);
-	log("buffer", file.buffer);
 
 	const result = await fetch(uploadURL, {
 		method: "POST",
 		headers: { "Content-Type": "audio/wav" },
 		body: file.buffer,
 	});
-	log("**************\n\n\n");
 	const { storageId } = (await result.json()) as { storageId: string };
-
-	log(storageId);
+	log(
+		`uploaded [${filename}] ${file.length} bytes to [${uploadURL}]. storageId = [${storageId}]`,
+	);
 }
