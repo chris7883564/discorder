0597d2bc8699541e91ebae12212a437731744011
chris7883564 <chris@musecopilot.com>
2024-04-27 20:09:42 -0700

    .

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,5 @@ import { BOT_TOKEN } from "./config";
 
 bot.login(BOT_TOKEN);
 
-import { uploadFileToConvex } from "./recorder/convexuploader";
-// test1().then(console.log);
-uploadFileToConvex("./data/recording/1714273057501/1007158516705927243/00000524.wav").then(
-	console.log,
-);
+// import { uploadFileToConvex } from "./recorder/convexuploader";
+// uploadFileToConvex("./data/recording/1714273057501/1007158516705927243/00000524.wav")
