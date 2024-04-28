84c269e2d2fac9f64470b342b17078d93a380999
chris7883564 <chris@musecopilot.com>
2024-04-28 10:41:24 -0700

    tried to add replicate but failed

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,7 +15,6 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as stems from "../stems.js";
-import type * as tasks from "../tasks.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -27,7 +26,6 @@ import type * as tasks from "../tasks.js";
  */
 declare const fullApi: ApiFromModules<{
   stems: typeof stems;
-  tasks: typeof tasks;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
--- convex/stems.ts
+++ convex/stems.ts
@@ -4,31 +4,20 @@ export const list = query(async (ctx) => {
 	return await ctx.db.query("stems").collect();
 });
 
-export const send = mutation(async (ctx, { body, author }) => {
-	const message = { body, author };
-	await ctx.db.insert("stems", message);
-});
-
-export const sendAudio = mutation(async (ctx, { storageId, author }) => {
-	const payload = { body: storageId, author, format: "audio/wav" };
-	await ctx.db.insert("stems", payload);
-});
-
 export const generateUploadUrl = mutation(async (ctx) => {
 	return await ctx.storage.generateUploadUrl();
 });
 
 export const createStem = mutation(async (ctx, args) => {
-	return await ctx.db.insert("stems", args);
-});
+	const stemID = await ctx.db.insert("stems", args);
 
-// export const sendImage = mutation({
-//   args: { storageId: v.id("_storage"), author: v.string() },
-//   handler: async (ctx, args) => {
-//     await ctx.db.insert("messages", {
-//       body: args.storageId,
-//       author: args.author,
-//       format: "image",
-//     });
-//   },
-// });
+	// const _storageID: Id<"_storage"> = args.storageId as Id<"_storage">;
+	// let fileUrl = (await ctx.storage.getUrl(_storageID)) as string;
+
+	// await ctx.scheduler.runAfter(0, internal.transcribe, {
+	// 	fileUrl,
+	// 	id: noteId,
+	// });
+
+	return stemID;
+});
