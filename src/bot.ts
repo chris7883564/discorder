433eec828e4963d23eaab7d77166f9fa36d7d982
chris7883564 <chris@musecopilot.com>
2024-07-23 22:03:50 +1000

    testing guildCreate message

--- src/bot.ts
+++ src/bot.ts
@@ -7,7 +7,7 @@ import Logger from "@/logger";
 const logger = new Logger("bot");
 
 //---------------------------------------------------------------------
-export const bot = new Client({ intents: ["GuildVoiceStates", "Guilds"] });
+export const client = new Client({ intents: ["GuildVoiceStates", "Guilds"] });
 
 //---------------------------------------------------------------------
 async function postLinkDiscorder(username: string) {
@@ -30,13 +30,37 @@ async function postLinkDiscorder(username: string) {
 }
 
 //---------------------------------------------------------------------
-bot.on("ready", () => {
-  logger.info(`Logged in as ${bot.user?.tag}`);
-  postLinkDiscorder(bot.user ? bot.user.tag : "unknown").catch(console.error);
+client.on("ready", () => {
+  logger.info(`client logged in as ${client.user?.tag}`);
+  postLinkDiscorder(client.user ? client.user.tag : "unknown").catch(
+    console.error,
+  );
 });
 
 //---------------------------------------------------------------------
-bot.on("interactionCreate", (interaction) => {
+client.on("guildCreate", async (guild) => {
+  console.log(`Bot added to guild: ${guild.name} (id: ${guild.id}).`);
+  console.log(guild);
+  // try {
+  //   const serverUrl = 'https://your-app-server.com/api/bot-added';
+  //   const data = {
+  //     guild_id: guild.id,
+  //     guild_name: guild.name,
+  //   };
+
+  //   const response = await axios.post(serverUrl, data);
+  //   if (response.status === 200) {
+  //     console.log('Successfully notified app server host.');
+  //   } else {
+  //     console.error('Failed to notify app server host:', response.statusText);
+  //   }
+  // } catch (error) {
+  //   console.error('Error notifying app server host:', error);
+  // }
+});
+
+//---------------------------------------------------------------------
+client.on("interactionCreate", (interaction) => {
   if (!interaction.isCommand()) {
     return;
   }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { bot } from "./bot";
+import { client } from "./bot";
 import { BOT_TOKEN } from "./config";
 
-bot.login(BOT_TOKEN);
+client.login(BOT_TOKEN);
 
 // import { uploadFileToConvex } from "./recorder/convexuploader";
 // uploadFileToConvex("./data/recording/1714273057501/1007158516705927243/00000524.wav")
