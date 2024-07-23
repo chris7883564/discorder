c4d6b00848fec2f5a54182ff533284f038cfc5b6
chris7883564 <chris@musecopilot.com>
2024-07-23 10:34:21 +1000

    updated error logging in uploadFileToConvex

--- src/recorder/convexuploader.ts
+++ src/recorder/convexuploader.ts
@@ -1,14 +1,25 @@
 // "use node";
 
-import debug from "debug";
-const log = debug("convexuploader");
-log.enabled = true;
+// import debug from "debug";
+// const baseNamespace = "convexuploader";
+// const logInfo = debug(`${baseNamespace}:info`);
+// const logWarn = debug(`${baseNamespace}:warn`);
+// const logError = debug(`${baseNamespace}:error`);
+// logInfo.enabled = true;
+// logWarn.enabled = true;
+// logError.enabled = true;
+// To enable only error logs:
+// DEBUG=convexuploader:error node yourApp.js
+// DEBUG=convexuploader:warn,convexuploader:error node yourApp.js
 
+const logger = new Logger("convexuploader");
+
+import Logger from "@/logger";
 import "../envConfig";
 import fs from "node:fs";
 
 const convexSiteUrl = process.env.CONVEX_SITE_URL;
-log("CONVEX_SITE_URL = " + convexSiteUrl);
+logger.info("CONVEX_SITE_URL = " + convexSiteUrl);
 
 // --------------------------------------------------------------------------------------------------
 export async function uploadFileToConvex(
@@ -32,16 +43,16 @@ export async function uploadFileToConvex(
     uploadURL.searchParams.set("talker_id", talker_id);
     uploadURL.searchParams.set("channel_id", channel_id);
     uploadURL.searchParams.set("time_offset", String(time_offset));
-    log(filename);
-    log(uploadURL.href);
+    logger.info(filename);
+    logger.info(uploadURL.href);
 
     const result = await fetch(uploadURL, {
       method: "POST",
       headers: { "Content-Type": "audio/wav" },
       body: file.buffer,
     });
-    log(result.status, file.buffer.byteLength);
+    logger.info(result.status, file.buffer.byteLength);
   } catch (e) {
-    log("error: ", e);
+    logger.error("error: ", e);
   }
 }
